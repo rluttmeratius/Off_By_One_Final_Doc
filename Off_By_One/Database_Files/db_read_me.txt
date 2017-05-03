47d5d3c8df44e81c16fa67fa3d13783e5953f0d2
Rebecca Luttmer <rebecca.luttmer@gmail.com>
2017-05-02 22:31:14 -0400

    added final folder

---
+++ Off_By_One/Database_Files/db_read_me.txt
@@ -0,0 +1,11 @@
+********************************
+The .csv files contain all of the 
+table row data, and the .sql scripts 
+will create new tables on your 
+system when you run them with the 
+mysql source command.  These are both
+ found in the source folder. Included 
+in this folder is the mysql database
+ audeme, including separate tables
+ for the game and dictionary.  
+********************************
---
+++ Off_By_One/Src/about.php
@@ -0,0 +1,59 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+
+    <title>Audeme - Home</title>
+
+    <?php include_once 'includes/universalCSS.php' ?>
+
+</head>
+<body>
+
+<?php include_once 'includes/navigation.php' ?>
+
+<h1 tabindex="0">Let us know what you think about the whole thing by contacting us.</h1>
+
+<h3 tabindex="0">Steve Mannheimer</h3>
+<p tabindex="0" class="text-left">Steve Mannheimer has been a faculty member at the Herron School of Art, IUPUI, since 1976. In the summer of 2000, he
+    began a two-year leave of absence from IUPUI to become a full-time employee of Thomson Multimedia, Inc. Within
+    Thomson's New Media Services division, he was part of a multi-disciplinary team developing new media business
+    concepts, strategies, business plans and technologies. His team worked in collaboration with other Thomson divisions
+    as well as with other research facilities around the world. While employed with Thomson, Mannheimer's work included
+    invention of patentable technologies and applications, resulting in over 20 patent disclosures filed either
+    individually or as part of the team.
+    <br>
+    In the spring of 2002, while still with Thomson, Mannheimer taught a seminar course for the Media Arts and Science
+    program in Imaging in Digital Media. In the summer of 2002, he returned to a full-time academic career, joining the
+    Media Arts and Science program in the School of Informatics and Computing.
+    <br>
+    In addition to his activities as a studio artist and university professor, Mannheimer has been an art journalist,
+    writing a weekly column on art and architecture for The Indianapolis Star for nearly 20 years. He has also been
+    involved in the civic promotion of art, having directed several large-scale public art projects in Indianapolis,
+    including the memorial to Dr. Martin Luther King, Jr. and Sen. Robert Kennedy in the city's King Memorial Park.</p>
+<p><a tabindex="0" href="https://soic.iupui.edu/people/steve-mannheimer/" class="btn btn-primary" role="button">https://soic.iupui.edu/people/steve-mannheimer/</a>&nbsp;
+    <a tabindex="0" href="smannhei@iupui.edu" class="btn btn-default" role="button">smannhei@iupui.edu</a>
+</p>
+
+<h3 tabindex="0">Larry Skutchan</h3>
+<p tabindex="0" class="text-left">Director of Technology Product Research at American Printing House for the Blind, Inc.</p>
+<p><a tabindex="0" href="https://www.linkedin.com/in/larry-skutchan-b726b89/" class="btn btn-primary" role="button">https://www.linkedin.com/in/larry-skutchan-b726b89/</a>&nbsp;
+    <a tabindex="0" href="lskutchan@aph.org" class="btn btn-default" role="button">lskutchan@aph.org</a>
+</p>
+
+<h3 tabindex="0">Team member name</h3>
+<p>Autobiography</p>
+<p><a href="#" class="btn btn-primary" role="button">Website</a>&nbsp;<a href="#" class="btn btn-default" role="button">Contact</a>
+</p>
+
+<h3 tabindex="0">Team member name</h3>
+<p>Autobiography</p>
+<p><a href="#" class="btn btn-primary" role="button">Website</a>&nbsp;<a href="#" class="btn btn-default" role="button">Contact</a>
+</p>
+
+
+<footer>
+    Audemes.org
+
+</footer>
+</body>
+</html>
